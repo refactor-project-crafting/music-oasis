bd6ecd88100de704a989989f43de321c5d5fbb6d
Mario González <mario@mariogl.com>
2024-10-14 15:35:05 +0200

    Create data layer with one example disc

---
+++ src/records/data.ts
@@ -0,0 +1,16 @@
+import { Record } from "./types";
+
+const records: Record[] = [
+  {
+    id: 1,
+    title: "Sueño su boca",
+    artist: "Raúl",
+    type: "Digital",
+    price: 99.9,
+    originalPrice: 119.99,
+    isRecordOfTheMonth: false,
+    coverUrl: "raul.webp",
+  },
+];
+
+export default records;
